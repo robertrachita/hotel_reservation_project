c2a8a5fde57469da963b42e322d9dd90ffef8e8f
robertrachita <72192997+robertrachita@users.noreply.github.com>
2020-11-16 17:32:48 +0100

    Folder formattin
    >created the branching structure
    >added a .gitignore file for different systems
    >added a .htaccess ignore file (to hide the extension if hosted)
    >created an empty index page
    >created an empty, reset css page

---
+++ public_html/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>index</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+</head>
+
+</body>
+</html>
