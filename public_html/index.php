e795b841fc33c78c4bb5605ea9b7841a119fea1a
robertrachita <72192997+robertrachita@users.noreply.github.com>
2020-12-08 16:09:50 +0100

    Home page v0.2
    >Added some more changes to the homepage, still looks horrible
    >Moved the header and the footer to php files, not fully functional as of yet
    >Changed the extension of the index file to .php

---
+++ public_html/index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Arjen's Plak</title>
+    <link rel='stylesheet' href='css/styles.css' type="text/css">
+    <meta charset="utf-8">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src='js/script.js'></script>-->
+</head>
+
+<body>
+    <?php include 'php/header.php' ?>
+    <div class="searchbox">
+        <form action="" method="POST">
+            <input name="entryDate" type="date" placeholder="Arrival">
+            <input name="entryDate" type="date" placeholder="Departure">
+            <input name="submit" type="submit" value=" ">
+        </form>
+    </div>
+
+    <content>
+        <section>
+            <div class="imageBox" style="background: url(images/appartement_iemke_1.jpg); background-size: 100% 100%;">
+            </div>
+            </div>
+            <div class="information">
+                <div>
+                    <h2>Apartment 1</h2><br>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                </div>
+                <div class="button_info_right">
+                    <form action="#"></form>
+                    <input type="button" name="button_info" value="More Information">
+                    </form>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="information">
+                <div>
+                    <h2>Apartment 2</h2><br>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                </div>
+                <div class="button_info_left">
+                    <form action="#"></form>
+                    <input type="button" name="button_info" value="More Information">
+                    </form>
+                </div>
+            </div>
+            <div class="imageBox" style="background: url(images/appartement_iemke_1.jpg); background-size: 100% 100%;">
+            </div>
+        </section>
+    </content>
+
+    <?php include 'php/footer.php' ?>
+</body>
+
+</html>
---
+++ public_html/php/header.php
@@ -0,0 +1,11 @@
+<header>
+        <div class="menu">
+            <a href="#" id="navbar"><img id="headerImages" src="images/Hamburger_icon.jpg" alt="menu_item" width="50px" height="50px"></a>
+        </div>
+        <div class="banner">
+            <img src="images/banner2.png" alt="banner" width="568.42x" height="120px">
+        </div>
+        <div class="account">
+            <a href="#"><img id="headerImages" src="images/43931722-user-icon-account-button-on-dark-background.jpg" alt="account" width="50px" height="50px"></a>
+        </div>
+    </header>
