b6dafb46c3e43a1377b753a7a4dc7f57722119c0
robertrachita <72192997+robertrachita@users.noreply.github.com>
2020-12-09 13:29:58 +0100

    Added the database
    >created the database
    >Added a default admin user with the credentials
    -------------------------------------------
    admin@admin.com
    admin
    -------------------------------------------
    >added some entries for the apartments table

---
+++ db.sql
@@ -0,0 +1,84 @@
+CREATE DATABASE IF NOT EXISTS `hotel_system`  DEFAULT CHARACTER SET utf8mb4;
+USE `hotel_system`;
+
+CREATE TABLE IF NOT EXISTS `apartments` (
+    `apartment_id`      INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `name`              VARCHAR(50) NOT NULL,
+    `capacity`          TINYINT NOT NULL,
+    `price_night`       DOUBLE NOT NULL,
+    `price_week`        DOUBLE NOT NULL,
+    `price_weekend`     DOUBLE NOT NULL,
+    `discount`          TINYINT NOT NULL,
+    `image_path`        VARCHAR(50),
+
+    CONSTRAINT PK_apartment_id PRIMARY KEY (apartment_id)
+
+)ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `users`(
+    `user_id`           INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `last_name`         VARCHAR(50) NOT NULL,
+    `first_name`        VARCHAR(50) NOT NULL,
+    `date_of_birth`     DATE NOT NULL,
+    `country`           VARCHAR(50) NOT NULL,
+    `city`              VARCHAR(50),
+    `street`            VARCHAR(50),
+    `postal_code`       VARCHAR(15) NOT NULL,
+    `house_number`      INT,
+    `telephone_number`  VARCHAR(15) NOT NULL,
+    `email`             VARCHAR(255) UNIQUE NOT NULL,
+    `password`          VARCHAR(255) NOT NULL,
+    `authorisation`     BOOLEAN NOT NULL DEFAULT '0',       
+
+    INDEX (`email`, `password`,`authorisation`),
+    CONSTRAINT PK_user_id PRIMARY KEY (user_id)
+
+)ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `orders`(
+    `order_id`          INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `date_start`        DATE NOT NULL,
+    `date_end`          DATE NOT NULL,
+    `status`            ENUM('PENDING', 'ACCEPTED', 'DECLINED') NOT NULL DEFAULT 'PENDING',
+    `total_price`       DOUBLE NOT NULL,
+    `apartment_id`      INT,
+    `user_id`           INT,
+ 
+    CONSTRAINT PK_order_id PRIMARY KEY (order_id),
+    CONSTRAINT FK_apartment_id FOREIGN KEY (apartment_id)
+                                REFERENCES apartments (apartment_id)
+                                ON UPDATE CASCADE
+                                ON DELETE NO ACTION,
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id)
+                          REFERENCES users (user_id)
+                          ON UPDATE CASCADE
+                          ON DELETE NO ACTION                         
+
+)ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `contact` (
+    `contact_id`        INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `message`           VARCHAR(255) NOT NULL,
+    `response`          VARCHAR(255),
+    `date`              DATE NOT NULL,
+    `user_id_contact`   INT,
+
+    CONSTRAINT PK_contact_id PRIMARY KEY (contact_id),
+    CONSTRAINT FK_user_id_contact FOREIGN KEY (user_id_contact)
+                                REFERENCES users (user_id)
+                                ON UPDATE CASCADE
+                                ON DELETE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/* Appartment information*/
+INSERT INTO `apartments`(`name`,`capacity`) values 
+('Iemke', 2),
+('Jaakje', 2),
+('Sil', 3),
+('Jelle', 4),
+('Wietse', 4),
+('Lobke', 5),
+('Maam', 6);
+/* default bcrypt hash, password is admin, for developing purposes only*/
+INSERT INTO `users`(`last_name`, `first_name`, `country`, `city`, `email`, `password`, `authorisation`) values
+    ('Peters', 'Victor', 'Netherlands', 'Groningen', 'admin@admin.com', '$2y$12$DznTtqjtDvfT6RvFfQdL7OmU1l4fsycMhVD38yG7eCO16v3jLzPKy', '1');
