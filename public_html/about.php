84fe843c9fceab2e12a4bd4de0da4634da93624d
zsumareh1 <52489517+zsumareh1@users.noreply.github.com>
2020-12-11 09:26:05 +0100

    About page
    about page

---
+++ public_html/about.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <title>About Us</title>
+  </head>
+  <body>
+      <?php include 'php/header.php' ?>
+
+    <div class="wrapper">
+
+      <div class="container">
+
+         <div class="lefttxt">
+           <p>We have a chain of quality appartements in Terschielling. We do everything we can to make sure you come back again. Our appartements are as different as our people. Nevertheless, we are sure that you will have an unforgettable stay everywhere; whether you have to meet with full concentration, arrive late at the appartemnt or get up early. Arjen's Plak guarantees the best moments in the most beautiful Island of the Netherlands. Are you looking for our company details? You can find it here.</p>
+         </div>
+
+         <div class="img">
+           <p><img src="images/foto.jpg" alt="roomfoto"></p>
+         </div>
+
+      </div>
+
+      <div class="main">
+
+        <div class="maintxt">
+
+           <h1>WHAT DO WE THINK IS IMPORTANT?</h1>
+
+           <div class="">
+             <p>The behavior of our employees is what connects our Values. We have translated this into our brand values, the 5 O's. How we explain this? You better come by. We do everything we can to make you come back again.</p>
+           </div>
+
+           <div class="">
+             <p><em>Sincerely guest-oriented</em> : Make a guest feel so welcome; that he feels more at home than a guest.</p>
+             <p><em>Straightforward</em> : Many problems can be solved by not turning them into a problem.</p>
+             <p><em>Entrepreneurship</em> : Much more fun than trial and error, standing and standing out.</p>
+             <p><em>Reversing</em> : Does it not work all at once? Three times the left is also the right.</p>
+             <p><em>Out of the box</em> : Where nothing is allowed to fail, nothing new can arise.</p>
+           </div>
+
+           <div class="">
+             <p><b>CORPORATE SOCIAL RESPONSIBILITY</b>
+                 We are aware of the impact that our business activities have on the environment and society. That is why we integrate sustainability into our business processes, services and products. </p>
+           </div>
+        </div>
+      </div>
+
+    </div>
+
+       <?php include 'php/footer.php' ?>
+  </body>
+</html>
