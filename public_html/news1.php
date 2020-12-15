d1f5e0ae997c37bbcb936f6282abe775b2d709b0
christostevan <72568727+christostevan@users.noreply.github.com>
2020-12-15 13:00:34 +0100

    Make a few changes for articles
    add a new page

---
+++ public_html/news1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src='js/script.js'></script>
+</head>
+<body>
+    <?php include 'php/header.php' ?>
+    <div class="news_content">
+        <h1>Vaccine for Covid-19</h1>
+        <div class="content_main">
+            <div class="cleft">
+                <img src="images/covid.jpg">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas aliquid ex soluta similique, debitis sint quidem tempore itaque sequi. Odit repudiandae pariatur hic praesentium perferendis porro sed voluptatibus accusamus at molestias rerum sequi, itaque vitae excepturi ipsam optio repellat exercitationem! Facilis odio eum ullam aperiam error debitis consequatur, quis ipsum.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius id deleniti repellat nihil magni suscipit qui, quis sunt cum possimus rerum consequuntur libero porro eveniet neque mollitia molestiae reiciendis. Aspernatur aliquid eligendi harum quaerat quis cumque corporis. Ad, quasi labore? Porro alias ullam quod fugit quo pariatur quibusdam, sunt consectetur!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, fuga alias voluptates fugit earum adipisci nisi consequuntur rem commodi eaque.</p>
+                <h3>Lorem Ipsum</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed minima aperiam itaque dolorum voluptates, maiores soluta quos assumenda facere impedit maxime amet in reiciendis a fuga inventore vel tempora sunt explicabo cumque cupiditate est veritatis! Natus magnam quidem nihil facilis facere dolores omnis ex eum officia! Reiciendis maiores, ipsa quod possimus odit fugiat perferendis quas repellendus minima quae assumenda voluptates.
+                     Explicabo eum quasi iure eveniet at sint laborum omnis temporibus suscipit laboriosam sunt est impedit dignissimos adipisci quae, veritatis aliquid eos. Laudantium repellendus natus mollitia doloremque excepturi. Vel sit ut eveniet voluptatibus id voluptatum eius repudiandae sequi. Voluptas, optio ea?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, deserunt magni modi deleniti, dolores est commodi quod in ab animi inventore sint facere nihil obcaecati facilis, tenetur asperiores corporis eos excepturi voluptatem labore iure nam. Perferendis animi officiis fugit sequi eligendi similique magnam dolores exercitationem quo ea. Minus totam voluptatum ipsam, modi maiores dicta explicabo dolorum, quaerat veritatis ut earum!</p>
+            </div>
+            <div class="cright">
+                <h1>Covid News</h1>
+                <div class="right_news">
+                    <img src="images/covid.jpg">
+                    <h2>Vaccine for Covid-19</h2>
+                </div>
+                <br><br>
+                <div class="right_news">
+                    <img src="images/covid-19.jpg">
+                    <h2>Covid-19 Cases in Netherlands</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'php/footer.php' ?>
+</body>
+</html>
